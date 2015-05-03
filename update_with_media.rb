1ccbc72e102ef1e9e3b5b1618467789f4244aa56
moguno <moguno@0kn.sakura.ne.jp>
2015-05-03 15:14:17 +0900

    block_given?よりblockがnilかどうかを見る方が好み。

--- update_with_media.rb
+++ update_with_media.rb
@@ -3,7 +3,7 @@
 class Service
   # 画像付きツイート
   def update_with_media(message, filenames, &block)
-    if block_given?
+    if block
       block.call(:start, [message, filenames])
       block.call(:try, [message, filenames])
     end
@@ -38,7 +38,7 @@ class Service
       message[:media_ids] = media_ids
 
       post(message) { |event, result|
-        if block_given?
+        if block
           case event
           when :success
             block.call(:success, result)
@@ -55,7 +55,7 @@ class Service
       puts e
       puts e.backtrace
 
-      if block_given?
+      if block
         block.call(:err, e)
         block.call(:fail, e)
         block.call(:exit, nil)
