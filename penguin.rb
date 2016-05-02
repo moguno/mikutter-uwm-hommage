fbd835137ae4cc2260b382cddfc13149751470fe
moguno <moguno@0kn.sakura.ne.jp>
2016-05-02 20:37:29 +0900

    最近使ったファイルのファイルがすでに消されてた場合、プレビューを表示できずに死ぬバグを修正した。

--- penguin.rb
+++ penguin.rb
@@ -42,7 +42,7 @@ def choose_image_file(multi_select = false)
   dialog.signal_connect("update-preview") {
     filename = dialog.preview_filename
     if filename
-      unless File.directory?(filename)
+      if FileTest.file?(filename) && FileTest.exist?(filename)
         pixbuf = Gdk::Pixbuf.new(filename, 128, 128)
         preview.set_pixbuf(pixbuf)
         dialog.set_preview_widget_active(true)
